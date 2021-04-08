f6df5e1c11192225e5bebd50d5b5f5ed17e568a2
Sindre Toft Nordal <sindretnordal@gmail.com>
2021-04-08 20:25:37 +0200

    Grammar

--- packages/frontend/src/components/Title.vue
+++ packages/frontend/src/components/Title.vue
@@ -18,7 +18,7 @@ export default Vue.extend({
       if (this.$store.state.currentRoute.name === "tasks")
         return "Velg aktiviteter";
       if (this.$store.state.currentRoute.name === "tokens")
-        return "Personlige access tokens";
+        return "Personlig access token";
       if (this.$store.state.currentRoute.name === "summarizedhours")
         return "Statistikk";
       if (this.$store.state.currentRoute.name === "accumulated-hours")
